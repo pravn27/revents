8b007d1784658399ec39e26f8498a1efc295c68a
pravn27 <praveen.sri27@gmail.com>
2021-04-10 07:40:48 +0530

    Added redux form dependency

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as FormReducer } from "redux-form";
 import eventReducer from "../../features/event/eventReducer";
 import testReducer from "./testReducer";
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   test: testReducer,
   events: eventReducer,
 });
